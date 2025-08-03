dc095af7f31a85a6520b2f62cebf6d75932d3537
Darya <ara1015@tut.by>
2025-08-03 18:05:53 +0300

    initial commit

--- project.py
+++ project.py
@@ -1,9 +1,12 @@
 import time
+import numpy as np
+from tqdm import tqdm
 
-total = 50
-count = 0
 
-while count < total:
-    count += 1
-    print("Line: " + str(count))
-    time.sleep(10)
+total = 100
+iter_tqdm = tqdm(range(total))
+
+for i in iter_tqdm:
+    #print(f"sin({i/100}) = {np.sin(i/100):.4f}")
+    iter_tqdm.set_description(f"x = ({i / 100}), sin = {np.sin(i / 100):.4f}")
+    time.sleep(1)
